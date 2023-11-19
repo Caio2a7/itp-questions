ae463030be57c0375e7c8c7948a377ccf814badb
Caio Daniel <127650414+Caio2a7@users.noreply.github.com>
2023-11-19 19:46:09 -0300

    Update URI_2126.c

---
+++ URI_2126.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_STR_LENGTH 100
+
+void subsequence_finder(const char *subsequence, const char *sequence) {
+    const char *substring = sequence;
+    while ((substring = strstr(substring, subsequence)) != NULL) {
+        printf("\nPos: %ld", substring - sequence);
+        substring++;
+    }
+}
+
+int main(void) {
+    char subsequence[MAX_STR_LENGTH], sequence[MAX_STR_LENGTH];
+    scanf("%s", subsequence);
+    getchar(); 
+    scanf("%s", sequence);
+
+    subsequence_finder(subsequence, sequence);
+    return 0;
+}
